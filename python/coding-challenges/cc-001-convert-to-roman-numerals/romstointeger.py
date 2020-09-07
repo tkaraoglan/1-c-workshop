c620df535bfd10b55e959be1a85d53ad366a4b60
karaoglan <tolgakaraoglan@gmail.com>
2020-09-07 18:56:27 +0300

    python çözümü düzeltildi.

--- python/coding-challenges/cc-001-convert-to-roman-numerals/romstointeger.py
+++ python/coding-challenges/cc-001-convert-to-roman-numerals/romstointeger.py
@@ -1,18 +1,19 @@
 
 def int_to_rome():
-    n = input("lütfen 0 ile 4000 arası bir sayı giriniz: \nçıkış için exit yazmanız yeterli.")
-    if not isinstance(n, (int)):
-        return "lütfen 1 ile 4000 arası bir sayı giriniz"
-    if not 0< n <4000:
-        return "lütfen belirtilen aralıkta bir sayı giriniz"
-    b = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
-    c = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
-    d= []
-    for i in range(len(b)):
-	    count = (n/b[i])
-	    d.append(count*c[i])
-	    n -= count * c[i]
-    return "".join(d)
-
+    try:
+        a = input("lütfen 0 ile 4000 arası bir sayı giriniz: \nçıkış için exit yazmanız yeterli.")
+        n = int(a)
+        if not 0 < n <4000:
+            return "lütfen belirtilen aralıkta bir sayı giriniz"
+        b = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+        c = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+        d= []
+        for i in range(len(b)):
+            count = int((n/b[i]))
+            d.append(count*c[i])
+            n -= count * b[i]
+        return "".join(d)
+    except ValueError:
+        return "lütfen tam sayı bir değer giriniz: "
 
 int_to_rome()
