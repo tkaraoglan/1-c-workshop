2f1ec1409d8b730cfab5936915d9ec6fd3f7190e
karaoglan <tolgakaraoglan@gmail.com>
2020-09-11 21:47:00 +0300

    flask çalışmalarının devamı 11.10

--- python/coding-challenges/cc-002-convert-millisecs-to-hours-mins-secs/mmtosecminhour.py
+++ python/coding-challenges/cc-002-convert-millisecs-to-hours-mins-secs/mmtosecminhour.py
@@ -5,7 +5,20 @@ while True:
     mint = sec*60
     hours = mint*60
     days = 24*hours
-
+    millis=input("Enter time in milliseconds ")
+    if millis.lower() == 'exit':
+        print("Exiting the program... Good Bye")
+    if not millis.isdigit():
+        print('please enter a valid number: ')
+    # time = input("Input time in seconds: ")
+    # day = time // (24 * 3600)
+    # time = time % (24 * 3600)
+    # hour = time // 3600
+    # time %= 3600
+    # minutes = time // 60
+    # time %= 60
+    # seconds = time
+    # print("d:h:m:s-> %d:%d:%d:%d" % (day, hour, minutes, seconds))
 
     #if not n.isdigit():
     #    return 'lütfen bir tam sayı giriniz'
@@ -13,18 +26,21 @@ while True:
     if n/milisec < 1:
         print('sadece milisaniye')
         break
-    elif n/milisec > 1 and n/milisec <sec:
+    elif n/milisec > 1 and n/milisec < sec:
         print('şu kadar seconds')
         break
     elif n/sec > 1 and n/sec < mint:
-        if n%60000 < 1:
+        if n%sec < 1:
             print("şu kadar dakika")
             break
         else:
             print("şu kadar dakika şu kadar saniye")
-    elif n/mint > 1 and n/mint < hours:
-        if n%60000 < 1:
-            print("şu kadar dakika")
             break
+    elif n/mint > 1 and n/mint < hours:
+        if n%mint < 1:
+            print("şu kadar saat")
+            break
+        elif n%mint > 1 and n%mint < :
+
         else:
             print("şu kadar dakika şu kadar saniye") 
---
+++ python/hands-on/flask-02-handling-routes-and-templates-on-ec2-linux2/app.py
@@ -0,0 +1,107 @@
+from flask import Flask, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'This is home page for no path, <h1> Welcome Home</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>This is my about page </h1>'
+
+@app.route('/error')
+def error():
+    return '<h1>Either you encountered an error or you are not authorized.</h1>' 
+
+@app.route('/hello')
+def hello():
+    return '<h1>Hello, World! </h1>'
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('error'))
+
+# @app.route('/<name>')
+# def greet(name):
+#    return f'hello, { name }'
+
+# @app.route('/<name>')
+# def greet(name):
+#     greet_format=f"""
+# <!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#     <title>Greeting Page</title>
+# </head>
+# <body>
+#     <h1> Hello { name }!</h1>
+#     <h1> Welcome to My greeting page!</h1>
+
+# </body>
+# </html>
+# """
+#     return greet_format
+
+@app.route('/<name>')
+def greet(name):
+    number1 = 23
+    number2 = 34
+    return render_template('greet.html', isim = name, number1 = 23, number2 = 34, toplam = number1 + number2)
+
+@app.route('/greet-admin')
+def greet_admin():
+    return redirect(url_for('greet', name = 'Master Admin!!!'))
+
+@app.route('/list100')
+def list100():
+    return render_template('list100.html')
+
+@app.route('/evens')
+def evens():
+    return render_template('evens.html')    
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+# Create a function named greet_admin which redirect the request to the hello path with param of 'Master Admin!!!!' 
+# and assign to the route of ('/greet-admin')
+
+# Rewrite a function named greet which which uses template file named `greet.html` under `templates` folder 
+# and assign to the dynamic route of ('/<name>')
+
+# Create a function named list10 which creates a list counting from 1 to 10 within `list10.html` 
+# and assign to the route of ('/list10')
+
+# Create a function named evens which show the even numbers from 1 to 10 within `evens.html` 
+# and assign to the route of ('/evens')
+
+# Add a statement to run the Flask application which can be reached from any host on port 80.
+# ```
+
+# - Write a template html file named `greet.html` which takes `name` as parameter under `templates` folder 
+
+# - Write a template html file named `list10.html` which shows a list counting from 1 to 10 under `templates` folder 
+
+# - Write a template html file named `evens.html` which shows a list of even numbers from 1 to 10 under `templates` folder 
+
+# - Create a folder named `static` under `hands-on/flask-02-handling-routes-and-templates-on-ec2-linux2` folder and create a text file named `mytext.txt` with *This is a text file in static folder* content.
+
+# - Add and commit all changes on local repo
+
+# - Push `app.py`, `greet.html`, `list10.html`, `evens.html`, and `mytext.txt` to remote repo `clarusway-python-workshop` on GitHub.
+
+# Part 4 - Run the Hello World App on EC2 Instance
+
+# - Download the web application file from GitHub repo.
+
+# - Run the web application
+
+# - Connect the route handling and templating web application from the web browser and try every routes configured
+
+# - Open the static file `mytext.txt` context from the web browser
+
+# - Connect the route handling and templating web application from the terminal with `curl` command.
