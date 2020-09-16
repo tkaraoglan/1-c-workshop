083d841ebaf51ac0572636f4b3aaefbe34aca4a8
karaoglan <tolgakaraoglan@gmail.com>
2020-09-16 22:14:52 +0300

    new sprint 16.08.2020

--- python/hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ python/hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -1,11 +1,10 @@
-from flask import  Flask
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World'
 
-if __name__ == "__main__":
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=80)
+if __name__=='__main__':
+   app.run(host='0.0.0.0', port=80)
